746b896289f0a4c2a28cd5d459d55223747d447a
Hosna <blackbutterfllly@gmail.com>
2024-01-28 17:28:49 +0330

    Add platformIconList component for displaying icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,16 @@
 ﻿import { useEffect, useState } from 'react';
 import apiClient, { CanceledError } from '../services/api-client.ts';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
