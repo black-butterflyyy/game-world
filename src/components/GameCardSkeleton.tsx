6068be3e03b861adc2f15adc867e1f7fc802e298
Hosna <blackbutterfllly@gmail.com>
2024-01-30 12:07:25 +0330

    Add game card skeleton for loading state

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,14 @@
+﻿import { Card, Skeleton, CardBody, SkeletonText } from '@chakra-ui/react';
+
+const GameCardSkeleton = () => {
+  return (
+    <Card borderRadius={10} overflow='hidden' width='300px'>
+      <Skeleton height='200px' />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
