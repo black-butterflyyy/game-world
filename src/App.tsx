3da21b0aa4ae51d70ad914aa7cff5fb666acd35b
Hosna <blackbutterfllly@gmail.com>
2024-02-06 12:49:39 +0330

    Refactor: simplify game query object

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
 import { Box, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
-import { Genre } from './hooks/useGenres';
-import { Platform } from './hooks/usePlatforms';
-import NavBar from './components/NavBar';
+import { useState } from 'react';
 import GameGrid from './components/GameGrid';
+import GameHeading from './components/GameHeading';
 import GenreList from './components/GenreList';
+import NavBar from './components/NavBar';
 import PlatformSelector from './components/PlatformSelector';
 import SortSelector from './components/SortSelector';
-import GameHeading from './components/GameHeading';
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -38,8 +36,10 @@ function App() {
       <Show above='lg'>
         <GridItem area='aside' paddingX={5}>
           <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            selectedGenreId={gameQuery.genreId}
+            onSelectGenre={(genre) =>
+              setGameQuery({ ...gameQuery, genreId: genre.id })
+            }
           />
         </GridItem>
       </Show>
@@ -49,9 +49,9 @@ function App() {
           <GameHeading gameQuery={gameQuery} />
           <HStack spacing={5} marginBottom={5}>
             <PlatformSelector
-              selectedPlatform={gameQuery.platform}
+              selectedPlatformId={gameQuery.platformId}
               onSelectPlatform={(platform) =>
-                setGameQuery({ ...gameQuery, platform })
+                setGameQuery({ ...gameQuery, platformId: platform.id })
               }
             />
             <SortSelector
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,10 +12,10 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenreId }: Props) => {
   const { data, error, isLoading } = useGenres();
 
   if (error) return null;
@@ -40,8 +40,8 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
                 fontSize='lg'
                 textAlign='left'
                 whiteSpace='normal'
-                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-                color={genre.id === selectedGenre?.id ? 'yellow' : ''}
+                fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'}
+                color={genre.id === selectedGenreId ? 'yellow' : ''}
                 variant='link'
                 onClick={() => onSelectGenre(genre)}
               >
