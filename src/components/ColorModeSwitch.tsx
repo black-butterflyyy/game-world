d5e48efe01ab162673038f299f0f1e9ca9422165
Hosna <blackbutterfllly@gmail.com>
2024-02-06 14:56:15 +0330

    Refactor: replace colormode switch with icons

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,17 +1,35 @@
-﻿import { Switch, Text, useColorMode, HStack } from '@chakra-ui/react';
+﻿import { useColorMode, IconButton } from '@chakra-ui/react';
+import { FaRegMoon } from 'react-icons/fa';
+import { FiSun } from 'react-icons/fi';
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
-  return (
-    <HStack>
-      <Switch
-        colorScheme='green'
-        isChecked={colorMode === 'dark'}
-        onChange={toggleColorMode}
+  if (colorMode === 'light')
+    return (
+      <IconButton
+        aria-label='Dark Mode'
+        icon={<FaRegMoon />}
+        bgColor='transparent'
+        isRound={true}
+        size='lg'
+        fontSize='25px'
+        color='rgba(52,58,70,.8)'
+        onClick={toggleColorMode}
       />
-      <Text whiteSpace='nowrap'>Dark Mode</Text>
-    </HStack>
+    );
+
+  return (
+    <IconButton
+      aria-label='Light Mode'
+      icon={<FiSun />}
+      bgColor='transparent'
+      isRound={true}
+      size='lg'
+      fontSize='25px'
+      color='white'
+      onClick={toggleColorMode}
+    />
   );
 };
 
