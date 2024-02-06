8c56dd9a2ff1ae1ad76ee51b5b387c68017246da
Hosna <blackbutterfllly@gmail.com>
2024-02-06 13:56:14 +0330

    Create lookup hooks

---
+++ src/hooks/usePlatform.ts
@@ -0,0 +1,8 @@
+﻿import usePlatforms from './usePlatforms';
+
+const usePlatform = (id?: number) => {
+  const { data: platforms } = usePlatforms();
+  return platforms?.results.find((p) => p.id === id);
+};
+
+export default usePlatform;
