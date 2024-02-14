d68d8c8159004431c4ed9595492e7d4d604cef7b
Hosna <blackbutterfllly@gmail.com>
2024-02-14 21:53:14 +0330

    Add game attributes component

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,54 @@
+﻿import { Text, SimpleGrid, Link } from '@chakra-ui/react';
+import DefinitionItem from './DefinitionItem';
+import CriticScore from './CriticScore';
+import { Game } from '../entities/Game';
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  return (
+    <SimpleGrid columns={2} as='dl'>
+      <DefinitionItem term='Platforms'>
+        {game.parent_platforms?.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefinitionItem>
+
+      <DefinitionItem term='Genres'>
+        {game.genres.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </DefinitionItem>
+
+      <DefinitionItem term='Publishers'>
+        {game.publishers?.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </DefinitionItem>
+
+      <DefinitionItem term='Metascore'>
+        <CriticScore score={game.metacritic} />
+      </DefinitionItem>
+
+      <DefinitionItem
+        term='Release date
+'
+      >
+        <Text>{game.released}</Text>
+      </DefinitionItem>
+
+      <DefinitionItem
+        term='Website
+'
+      >
+        <Link href={game.website} target='_blank'>
+          {game.website}
+        </Link>
+      </DefinitionItem>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,15 @@
-﻿import { Platform } from './Platform';
+﻿import { Genre } from './Genre';
+import { Platform } from './Platform';
+import { Publisher } from './Publisher';
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  released: string;
+  website: string;
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
