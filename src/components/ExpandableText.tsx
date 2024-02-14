b3e07d722b08f1a3c281dcf43e18534ca42eef3a
Hosna <blackbutterfllly@gmail.com>
2024-02-14 19:52:59 +0330

    Add expandable text component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,32 @@
+﻿import { useState } from 'react';
+import { Text, Button } from '@chakra-ui/react';
+
+interface Props {
+  children: string;
+  maxLength?: number;
+}
+
+const ExpandableText = ({ children, maxLength = 300 }: Props) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    
+    if (!children) return null;
+
+  if (children.length <= maxLength) return <Text>{children}</Text>;
+
+  return (
+    <Text>
+      {isExpanded ? children : children.substring(0, maxLength) + '....'}
+      <Button
+        onClick={() => setIsExpanded(!isExpanded)}
+        colorScheme='yellow'
+        size='xs'
+        fontWeight='bold'
+        marginLeft={1}
+      >
+        {isExpanded ? 'Show Less' : 'Read More'}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
