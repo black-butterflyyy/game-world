026ca65d635914359253a587d18ea899bd35b946
Hosna <blackbutterfllly@gmail.com>
2024-02-15 12:43:28 +0330

    Add game screenshots to detail page

---
+++ src/components/GameScreenshots.tsx
@@ -0,0 +1,29 @@
+﻿import { SimpleGrid, Image } from '@chakra-ui/react';
+import useGameScreenshots from '../hooks/useGameScreenshots';
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenshots = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useGameScreenshots(gameId);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid
+      columns={{
+        base: 1,
+        md: 2,
+      }}
+      spacing={5}
+    >
+      {data?.results.map((screenshot) => (
+        <Image key={screenshot.id} src={screenshot.image} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenshots;
---
+++ src/entities/Screenshot.ts
@@ -0,0 +1,6 @@
+﻿export interface Screenshot {
+  id: number;
+  image: string;
+  height: number;
+  width: number;
+}
