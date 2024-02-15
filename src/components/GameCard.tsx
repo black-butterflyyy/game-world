e9ffceaaa6721a4eaf30313550fcb92f52e7fd6a
Hosna <blackbutterfllly@gmail.com>
2024-02-15 12:15:08 +0330

    Add game trailer to detail page

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,24 +12,22 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Link to={`/games/${game.slug}`}>
-      <Card>
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
-          <HStack justifyContent='space-between' marginBottom={3}>
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-          m
-          <Heading fontSize='2xl'>
-            <Link to={`/games/${game.slug}`}> {game.name}</Link>
-            <Emoji rating={game.rating_top} />
-          </Heading>
-        </CardBody>
-      </Card>
-    </Link>
+    <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent='space-between' marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        m
+        <Heading fontSize='2xl'>
+          <Link to={`/games/${game.slug}`}> {game.name}</Link>
+          <Emoji rating={game.rating_top} />
+        </Heading>
+      </CardBody>
+    </Card>
   );
 };
 
---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import useGameTrailers from '../hooks/useGameTrailers';
+
+interface Props {
+  gameId: string | number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useGameTrailers(gameId);
+  const first = data?.results[0];
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
